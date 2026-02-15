fda8cc1a96bcde445598e120583a5775f92504d8
WatchDG <baranov@w3m.co>
2026-02-15 17:50:56 +0300

    chore: update version to 0.3.1 and enhance WireLength implementation for Vec

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,6 @@
+use std::iter::Sum;
+use std::ops::Add;
+
 /// Length of raw data in bytes in on-the-wire format.
 ///
 /// On-the-wire data is the byte representation used when data is sent over
@@ -5,4 +8,10 @@
 pub trait WireLength<T> {
     /// Returns the length of this value in bytes when encoded in wire format.
     fn wire_length(&self) -> T;
+}
+
+impl<T: WireLength<K>, K: Add<Output = K> + Sum<K>> WireLength<K> for Vec<T> {
+    fn wire_length(&self) -> K {
+        self.iter().map(|item| item.wire_length()).sum()
+    }
 }
