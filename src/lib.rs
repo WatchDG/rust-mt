515381fc0e2146032ce8141d5858c990ba7c7a5f
WatchDG <baranov@w3m.co>
2026-02-15 17:26:10 +0300

    feat: add WireLength

---
+++ src/lib.rs
@@ -0,0 +1,8 @@
+/// Length of raw data in bytes in on-the-wire format.
+///
+/// On-the-wire data is the byte representation used when data is sent over
+/// a network or stored in wire format, before any in-memory deserialization.
+pub trait WireLength<T> {
+    /// Returns the length of this value in bytes when encoded in wire format.
+    fn wire_length(&self) -> T;
+}
